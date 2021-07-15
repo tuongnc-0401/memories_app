dad25205d445cbbeee477ec8b720b38640045d36
tuongnc <s3818196@rmit.edu.vn>
2021-07-15 21:53:35 +0700

    update

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRouter);
 const CONNECTION_URL = 'mongodb+srv://tuong:tuong@cluster0.nm2wa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://huykiengabc9:huykiengabc9123@cluster0.qe11u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
