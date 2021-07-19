0f4b743e4a114875e7ca2552ce2f9502015b975d
tuongnc <s3818196@rmit.edu.vn>
2021-07-19 20:09:02 +0700

    Auth form

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import memories from './images/memories.png'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { useDispatch } from 'react-redux';
-
-import Posts from './components/Posts/Posts.js'
-import Form from './components/Form/Form.js'
-
-
-
-import useStyles from './styles.js'
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
+import { Container } from '@material-ui/core';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Auth from './components/Auth/Auth';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+
+
+
+
 
 const App = () => {
 
